0d4d23da032e48ba87ab75144357fb477441e300
Tri Phan <tripp@ames.edu.vn>
2023-03-22 17:32:52 +0700

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import HeroSection from './components/zero-hero/HeroSection'
+// import HeroSection from './components/zero-hero/HeroSection'
+
+import Alert from './components/series/Alert'
 
 function App() {
-	return <HeroSection />
+	return (
+		<div className='grid place-content-center w-screen h-80'>
+			<Alert />
+		</div>
+	)
+	// return <Alert />
+	// return <HeroSection />
 }
 
 export default App
---
+++ src/components/series/Alert.jsx
@@ -0,0 +1,32 @@
+import ErrorIcon from '@mui/icons-material/Error'
+
+const Alert = () => {
+	return (
+		<div className='flex items-center bg-blue-100 rounded-lg p-4 mb-4'>
+			<svg
+				class='w-5 h-5 text-blue-700'
+				fill='currentColor'
+				viewBox='0 0 20 20'
+				xmlns='http://www.w3.org/2000/svg'
+			>
+				<path
+					fill-rule='evenodd'
+					d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
+					clip-rule='evenodd'
+				></path>
+			</svg>
+			<p className='ml-3 text-sm text-blue-700'>
+				<span className='font-medium'>Important Note!</span> Change a few things
+				up and try to submit again.
+			</p>
+		</div>
+	)
+	// return (
+	// 	<p className='bg-blue-200 text-blue-500 p-4 font-medium rounded-lg'>
+	// 		<span className='text-blue-700'>Important Note!</span> Change a few things
+	// 		up and try to submit again.
+	// 	</p>
+	// )
+}
+
+export default Alert
